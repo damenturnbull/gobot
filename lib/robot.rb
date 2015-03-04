74449a943d837e2c505319bd4e58af6e024bbcdf
Damen Turnbull <info@fando.com.au>
2015-03-04 21:11:58 +1100

    Robot moves forward.

--- lib/robot.rb
+++ lib/robot.rb
@@ -6,5 +6,18 @@ class Robot
 		@x					= x
 		@y					= y
 		@placed			= false
-	end	
+	end
+
+	def move_forward
+		case @direction
+			when "NORTH"
+				@y += 1
+			when "EAST"
+				@x += 1
+			when "SOUTH"
+				@y -= 1
+			when "WEST"
+				@x -= 1
+		end
+	end
 end
--- spec/lib/robot_spec.rb
+++ spec/lib/robot_spec.rb
@@ -1,4 +1,5 @@
 describe Robot do 
+
 	describe '#initialize' do
 		let(:robot) { Robot.new("NORTH", 0, 0) }
 		it 'creates object' do
@@ -17,4 +18,28 @@ describe Robot do
 			expect(robot.placed).to be false
 		end
 	end
+
+	describe '#move_forward' do
+		it 'advances the robot North' do
+			robot = Robot.new("NORTH", 0,0)
+			robot.move_forward
+			expect(robot.y).to eq(1)
+		end
+		it 'advances the robot East' do
+			robot = Robot.new("EAST", 0,0)
+			robot.move_forward
+			expect(robot.x).to eq(1)
+		end
+		it 'advances the robot South' do
+			robot = Robot.new("SOUTH", 0,0)
+			robot.move_forward
+			expect(robot.y).to eq(-1)
+		end
+		it 'advances the robot West' do
+			robot = Robot.new("WEST", 0,0)
+			robot.move_forward
+			expect(robot.x).to eq(-1)
+		end
+	end
+
 end
