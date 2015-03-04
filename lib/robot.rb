8dfa1fd6fab4b4da697caf6cff71e42534101be8
Damen Turnbull <info@fando.com.au>
2015-03-04 20:50:34 +1100

    Robot initialized.

--- lib/robot.rb
+++ lib/robot.rb
@@ -1,2 +1,10 @@
 class Robot
+	attr_accessor :direction, :x, :y, :placed
+
+	def initialize(direction, x, y)
+		@direction	= direction
+		@x					= x
+		@y					= y
+		@placed			= false
+	end	
 end
--- spec/lib/robot_spec.rb
+++ spec/lib/robot_spec.rb
@@ -1,7 +1,20 @@
 describe Robot do 
-	describe 'initialize' do
-		it 'does just that.' do
-			expect(Robot.new).not_to be nil
+	describe '#initialize' do
+		let(:robot) { Robot.new("NORTH", 0, 0) }
+		it 'creates object' do
+			expect(robot).not_to be nil
+		end
+		it 'sets direction' do
+			expect(robot.direction).to eq("NORTH")
+		end
+		it 'sets x' do
+			expect(robot.x).to eq(0)
+		end
+		it 'sets y' do
+			expect(robot.y).to eq(0)
+		end
+		it 'sets placed to false' do
+			expect(robot.placed).to be false
 		end
 	end
 end
