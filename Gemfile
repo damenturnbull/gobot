1c2e3e5777b9362a74c824d7521fe6b817fbc857
Damen Turnbull <info@fando.com.au>
2015-03-04 17:03:56 +1100

    Include rspec. Add require_all. Prove first model test.

---
+++ Gemfile
@@ -0,0 +1,5 @@
+source 'https://rubygems.org'
+
+group :test do
+  gem 'require_all'
+end
---
+++ spec/lib/robot_spec.rb
@@ -0,0 +1,7 @@
+describe Robot do 
+	describe 'initialize' do
+		it 'does just that.' do
+			expect(Robot.new).not_to be nil
+		end
+	end
+end
